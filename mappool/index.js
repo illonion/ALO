a7d511a5c8c8d809dab6212846b5b36bac89bfab
Lawrence Zhou <60532041+illonion@users.noreply.github.com>
2025-06-08 13:54:26 +1000

    Complete team name, seed, players, and mod rankings

--- mappool/index.js
+++ mappool/index.js
@@ -1,11 +1,70 @@
+// Get Team
+let allTeams
+async function getTeam() {
+    const response = await axios.get("../_data/teams.json")
+    allTeams = response.data
+}
+getTeam()
+// Find Team
+const findTeam = team_name => allTeams.find(team => team.team_name === team_name)
+
+// Team Information
+const teamNameLeftEl = document.getElementById("team-name-left")
+const teamNameRightEl = document.getElementById("team-name-right")
+const teamPlayersLeftEl = document.getElementById("team-players-left")
+const teamPlayersRightEl = document.getElementById("team-players-right")
+const teamSeedLeftEl = document.getElementById("team-seed-left")
+const teamSeedRightEl = document.getElementById("team-seed-right")
+let currentTeamNameLeft, currentTeamNameRight
+
 // osu! chat length
 const osuChatDisplayContainerEl = document.getElementById("osu-chat-display-container")
 let chatLength
+
+// Set team details
+function setTeam(team, teamPlayersElement, teamSeedElement, side) {
+    console.log(team)
+    
+    // Set Players Element
+    for (let i = 0; i < teamPlayersElement.childElementCount; i++) {
+        teamPlayersElement.children[i].children[0].setAttribute("src", `https://a.ppy.sh/${team.player_ids[i]}`)
+        teamPlayersElement.children[i].children[1].children[0].textContent = team.player_names[i]
+        teamPlayersElement.children[i].children[1].children[1].textContent = `#${team.player_ranks[i].toLocaleString()}`
+    }
+
+    // Set Seed Element
+    teamSeedElement.textContent = `#${team.seed.toString().padStart(2, "0")}`
+
+    // Set Mod Ranks
+    const modElements = document.getElementsByClassName(`team-mod-number-${side}`)
+    for (let i = 0; i < team.mod_ranks.length; i++) {
+        console.log
+        modElements[i].textContent = `#${team.mod_ranks[i]}`
+    }
+}
 
 const socket = createTosuWsSocket()
 socket.onmessage = event => {
     const data = JSON.parse(event.data)
     console.log(data)
+
+    // Set Team Data
+    if (currentTeamNameLeft !== data.tourney.team.left && allTeams) {
+        currentTeamNameLeft = data.tourney.team.left
+        teamNameLeftEl.textContent = currentTeamNameLeft
+
+        const foundTeam = findTeam(currentTeamNameLeft)
+        if (foundTeam) setTeam(foundTeam, teamPlayersLeftEl, teamSeedLeftEl, "left") 
+    }
+    if (currentTeamNameRight !== data.tourney.team.right && allTeams) {
+        console.log(data.tourney.team.right)
+        currentTeamNameRight = data.tourney.team.right
+        console.log(currentTeamNameRight)
+        teamNameRightEl.textContent = currentTeamNameRight
+
+        const foundTeam = findTeam(currentTeamNameRight)
+        if (foundTeam) setTeam(foundTeam, teamPlayersRightEl, teamSeedRightEl, "right") 
+    }
 
     // This is mostly taken from Victim Crasher: https://github.com/VictimCrasher/static/tree/master/WaveTournament
     if (chatLength !== data.tourney.chat.length) {
