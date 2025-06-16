99469a37878c25305c033c7b233ef1e643f9ba41
Lawrence Zhou <60532041+illonion@users.noreply.github.com>
2025-06-16 11:31:59 +1000

    Add round name dynamically

---
+++ main/index.js
@@ -0,0 +1,9 @@
+const roundNameEl = document.getElementById("round-name")
+
+let allBeatmaps
+async function getBeatmaps() {
+    const response = await axios.get("../_data/beatmaps.json")
+    allBeatmaps = response.data.beatmaps
+    roundNameEl.setAttribute("src", `static/rounds/${response.data.roundName}.png`)
+}
+getBeatmaps()
