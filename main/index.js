583974781ceb161ac456445beb7c5517fe102ba3
Lawrence Zhou <60532041+illonion@users.noreply.github.com>
2025-06-16 13:43:23 +1000

    Add logic for client information

--- main/index.js
+++ main/index.js
@@ -1,6 +1,16 @@
-const roundNameEl = document.getElementById("round-name")
+// Get Team
+let allTeams, leftTeam, rightTeam
+async function getTeams() {
+    const response = await axios.get("../_data/teams.json")
+    allTeams = response.data
+}
+getTeams()
+// Find Team
+const findTeam = team_name => allTeams.find(team => team.team_name === team_name)
+const findPlayerRankIndex = (player_id, team) => team.player_ids.findIndex(player_id)
 
 // Get beatmaps
+const roundNameEl = document.getElementById("round-name")
 let allBeatmaps
 async function getBeatmaps() {
     const response = await axios.get("../_data/beatmaps.json")
@@ -17,17 +27,65 @@ const teamNameLeftEl = document.getElementById("team-name-left")
 const teamNameRightEl = document.getElementById("team-name-right")
 let currentTeamNameLeft, currentTeamNameRight
 
+// Client information
+const leftPlayerInformationEl = document.getElementById("gameplay-section-left-player-information")
+const rightPlayerInformationEl = document.getElementById("gameplay-section-right-player-information")
+let leftPlayerId, rightPlayerId
+
 socket.onmessage = event => {
     const data = JSON.parse(event.data)
     console.log(data)
 
-    if (currentTeamNameLeft !== data.tourney.team.left) {
+    // Team Names
+    if (currentTeamNameLeft !== data.tourney.team.left && allTeams) {
         currentTeamNameLeft = data.tourney.team.left
         teamNameLeftEl.textContent = currentTeamNameLeft
+        leftTeam = findTeam(currentTeamNameLeft)
     }
-    if (currentTeamNameRight !== data.tourney.team.right) {
+    if (currentTeamNameRight !== data.tourney.team.right && allTeams) {
         currentTeamNameRight = data.tourney.team.right
         teamNameRightEl.textContent = currentTeamNameRight
+        rightTeam = findTeam(currentTeamNameRight)
+    }
+
+    // Client Information
+    // Client 0
+    if (leftPlayerId !== data.tourney.clients[0].user.id && allTeams) {
+        leftPlayerId = data.tourney.clients[0].user.id
+        setPlayerInformation(leftPlayerId, leftTeam, leftPlayerInformationEl, data.tourney.clients[0])
+    }
+    // Client 1
+    if (rightPlayerId !== data.tourney.clients[1].user.id && allTeams) {
+        rightPlayerId = data.tourney.clients[1].user.id
+        setPlayerInformation(rightPlayerId, rightTeam, rightPlayerInformationEl, data.tourney.clients[1])
+    }
+}
+
+// Set Player Information
+function setPlayerInformation(playerId, playerTeam, playerElement, clientData) {
+    playerElement.children[0].setAttribute("src", `https://a.ppy.sh/${playerId}`)
+    playerElement.children[1].children[0].textContent = clientData.user.name
+
+    if (playerTeam) {
+        // Set player rank index
+        const playerRankIndex = findPlayerRankIndex(playerId, playerTeam)
+        if (playerRankIndex) {
+            playerElement.children[1].children[1].textContent = `#${playerTeam.player_ranks[playerRankIndex]}`
+        } else {
+            playerElement.children[1].children[1].textContent = `#${clientData.user.globalRank}`
+        }
+        // Set Seed
+        playerElement.children[1].children[2].style.display = "block"
+        playerElement.children[1].children[2].textContent = `SEED ${playerTeam.seed}`
+    } else {
+        playerElement.children[1].children[1].textContent = `#${clientData.user.globalRank}`
+        playerElement.children[1].children[2].style.display = "none"
+    }
+
+    if (!leftPlayerId) {
+        playerElement.style.display = "none"
+    } else {
+        playerElement.style.display = "flex"
     }
 }
 
@@ -79,6 +137,6 @@ function createStarDisplay() {
 
     // Set cookies
     document.cookie = `currentTeamStarLeft=${currentTeamStarLeft}; path=/`
-    document.cookie = `currentTeamStarRight=${currentTeamStarRight}; path=/`
+    document.cookie = `currentTeamStarRight=${currentTeamStarRight};    path=/`
     document.cookie = `currentFirstTo=${currentFirstTo}; path=/`
 }
