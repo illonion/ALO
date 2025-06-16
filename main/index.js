77b7bbea590c75af432be33d82c9bb68312d0c1c
Lawrence Zhou <60532041+illonion@users.noreply.github.com>
2025-06-16 11:36:02 +1000

    Add team name

--- main/index.js
+++ main/index.js
@@ -1,5 +1,6 @@
 const roundNameEl = document.getElementById("round-name")
 
+// Get beatmaps
 let allBeatmaps
 async function getBeatmaps() {
     const response = await axios.get("../_data/beatmaps.json")
@@ -7,3 +8,25 @@ async function getBeatmaps() {
     roundNameEl.setAttribute("src", `static/rounds/${response.data.roundName}.png`)
 }
 getBeatmaps()
+
+// Socket
+const socket = createTosuWsSocket()
+
+// Team Names
+const teamNameLeftEl = document.getElementById("team-name-left")
+const teamNameRightEl = document.getElementById("team-name-right")
+let currentTeamNameLeft, currentTeamNameRight
+
+socket.onmessage = event => {
+    const data = JSON.parse(event.data)
+    console.log(data)
+
+    if (currentTeamNameLeft !== data.tourney.team.left) {
+        currentTeamNameLeft = data.tourney.team.left
+        teamNameLeftEl.textContent = currentTeamNameLeft
+    }
+    if (currentTeamNameRight !== data.tourney.team.right) {
+        currentTeamNameRight = data.tourney.team.right
+        teamNameRightEl.textContent = currentTeamNameRight
+    }
+}
